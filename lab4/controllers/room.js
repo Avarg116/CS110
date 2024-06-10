3626c770fa3605adac8f86b1f0c03a3a1de799c4
Maker424 <jimmydelgado0812@gmail.com>
2024-06-10 13:49:45 -0700

    Include sanitization for room.js

--- lab4/controllers/room.js
+++ lab4/controllers/room.js
@@ -1,14 +1,15 @@
 const Message = require('../models/Message');
 const roomGenerator = require('../util/roomIdGenerator.js');
 const moment = require('moment');
+const sanitize = require('mongo-sanitize');
 
 async function getRoom(request, response) {
-  const roomName = request.params.roomName;
+  const roomName = sanitize(request.params.roomName);
   response.render('room', { title: 'chatroom', roomName: roomName, newRoomId: roomGenerator.roomIdGenerator() });
 }
 
 async function getMessages(request, response) {
-  const roomName = request.params.roomName;
+  const roomName = sanitize(request.params.roomName);
 
     const roomMessages = await Message.find({ roomId: roomName }).sort({ timestamp: 1 });
     response.json(roomMessages);
@@ -16,21 +17,24 @@ async function getMessages(request, response) {
 }
 
 async function postMessage(request, response) {
-  const roomName = request.params.roomName;
+  const roomName = sanitize(request.params.roomName);
+  const nickname = sanitize(request.body.nickname);
+  const body = sanitize(request.body.body); 
+
   const newMessage = new Message({
     roomId: roomName,
-    nickname: request.body.nickname,
-    body: request.body.body,
-    timestamp: moment().toDate() 
+    nickname: nickname,
+    body: body,
+    timestamp: moment().toDate()
   });
-    await newMessage.save();
 
+  await newMessage.save();
 }
 
 //
 async function editMessage(request, response) {
   const { roomId, messageId } = request.params;
-  const { body } = request.body;
+  const { body } = sanitize(request.body);
 
   const updatedMessage = await Message.findByIdAndUpdate(
     messageId,
@@ -58,8 +62,8 @@ async function deleteMessage(request, response) {
 }
 
 async function searchMessages(request, response) {
-  const roomName = request.params.roomName;
-  const query = request.query.query;
+  const roomName = sanitize(request.params.roomName);
+  const query = sanitize(request.query.query);
 
   try {
     // Find messages
